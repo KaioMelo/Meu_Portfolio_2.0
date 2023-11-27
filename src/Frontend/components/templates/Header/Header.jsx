30afb1b23bd75a6105394c9a7783f2df070467b1
Kaio Melo dos Santos <kaiomelo1998@gmail.com>
2023-11-26 21:48:19 -0300

    Subindo fonts e ajustes no navbar do site

--- src/Frontend/components/templates/Header/Header.jsx
+++ src/Frontend/components/templates/Header/Header.jsx
@@ -1,18 +1,24 @@
 import './Header.css'
+import './Script.js'
 import { Link } from 'react-router-dom';
 
 function NavBar(){
     return(
-        <header>
+        <header> 
             <div className="logoName">
                 <h2>Kaio Melo</h2>
             </div>
+            <div className="menu-toggle" id="mobile-menu">
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
             <nav className="navbar">
-                <ul>
-                    <Link to='/' className='active'>Início</Link>
-                    <Link to=''>Sobre Mim</Link>
-                    <Link to=''>Projetos</Link>
-                    <Link to=''>Contatos</Link>
+                <ul className="nav-list">
+                    <li><a><Link to='/' className='active'>Início</Link></a></li>
+                    <li><a><Link to=''>Sobre Mim</Link></a></li>
+                    <li><a><Link to=''>Projetos</Link></a></li>
+                    <li><a> <Link to=''>Contatos</Link></a></li>
                 </ul>
             </nav>
         </header> 
